c7b554f9e8278b51d0872386eb037c8382ab01b4
Alex C <alex.m.calia@outlook.com>
2020-09-11 17:03:51 -0400

    working out data structure and functions for retrieving data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Board from "./components/Board";
 
-const App= () => {
+const App = () => {
   return (
     <div className="App">
-      
+      <Board />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from "react";
-import List from "./List";
-import TextInput from "./TextInput";
-import Item from "./Item";
-import Note from "./Note";
+// import List from "./List";
+// import TextInput from "./TextInput";
+// import Item from "./Item";
+// import Note from "./Note";
+import firebase from "../firebase";
+
+const dbRef = firebase.database().ref("board");
 
 const Board = () => {
-  return (
-    <ul className="board">
-      <List>
-        <Item>
-          <Note />
-          <TextInput className="noteInput" />
-        </Item>
-        <TextInput className="itemInput" />
-      </List>
-      <li>
-        <TextInput className="listInput" />
-      </li>
-    </ul>
-  );
+  const [items, setItems] = useState([]);
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    dbRef.on("value", (response) => {
+      const snap = response.val();
+      setLists(Object.keys(snap));
+      console.log(lists);
+      let snapItems = {};
+
+      lists.forEach((list) => {
+        let notes = [];
+        for (let note in snap[list]) {
+          notes.push(snap[list][note]);
+        }
+        snapItems = { ...snapItems, [list]: notes };
+      });
+
+      setItems(snapItems);
+    });
+  }, []);
+
+  return <ul className="board"></ul>;
 };
 
 export default Board;
